6c1c2d34c1d88c9d9ecf0fcc947068b2f63aca38
Aleksey <loha.malyota@gmail.com>
2020-04-26 23:16:23 +0300

    refactor and add upload photos to database

--- src/test/java/com/instagram/download/photo/selenide/pages/UserPage.java
+++ src/test/java/com/instagram/download/photo/selenide/pages/UserPage.java
@@ -1,14 +1,12 @@
 package com.instagram.download.photo.selenide.pages;
 
-import org.apache.commons.io.IOUtils;
+import com.instagram.download.photo.selenide.databases.MariaDB;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -16,30 +14,30 @@ import java.util.Map;
 import static com.codeborne.selenide.Selenide.*;
 
 public class UserPage {
-    private static Logger logger = Logger.getLogger(UserPage.class);
+    private static final Logger logger = Logger.getLogger(UserPage.class);
     private static int count = 1;
     private int step = 250;
-    private int postsItems = setPostsItems();
-    private int savedPostsCount = 0;
+    private final int postItems = setPostsItems();
+    private final MariaDB db;
+
+    public UserPage(MariaDB db) {
+        this.db = db;
+    }
 
     private int setPostsItems() {
         logger.debug("try set posts items");
         return Integer.parseInt($(By.xpath("//li[contains(*, 'posts')]/span/span")).getText());
     }
 
-    public int getPostsItems() {
-        return postsItems;
-    }
-
-    public int getSavedPostsCount() {
-        return savedPostsCount;
+    public int getPostItems() {
+        return postItems;
     }
 
     public void downloadUserPhotos() {
         logger.info("try to download user photos");
         Map<WebElement, String> savedPhotos = new LinkedHashMap<>();
         logger.debug("put photos");
-        while (savedPhotos.size() < postsItems) {
+        while (savedPhotos.size() < postItems) {
             $$(By.xpath("//img[@class='FFVAD']")).forEach(webElement -> {
                 if (!savedPhotos.containsKey(webElement))
                     savedPhotos.put(webElement, webElement.getAttribute("src"));
@@ -52,9 +50,9 @@ public class UserPage {
 
     private void downloadSaved(Map<WebElement, String> savedPhotos) {
         savedPhotos.values()
-                .stream()
-                .map(this::transformURL)
-                .forEach(this::downloadPhoto);
+                  .stream()
+                  .map(this::transformURL)
+                  .forEach(this::writePhotoToDB);
     }
 
     private InputStream transformURL(String src) {
@@ -66,17 +64,9 @@ public class UserPage {
         }
     }
 
-    private void downloadPhoto(InputStream inputStream) {
+    private void writePhotoToDB(InputStream inputStream) {
         if (inputStream != null) {
-            try (OutputStream fileOutputStream = new FileOutputStream(ClassLoader.getSystemResource(".").getPath()
-                    + String.format("/photo%d.png", count++))) {
-                IOUtils.copy(inputStream, fileOutputStream);
-                inputStream.close();
-                savedPostsCount++;
-            } catch (IOException e) {
-                logger.error("File didn't write");
-                throw new RuntimeException("File didn't write " + e.getMessage());
-            }
+            db.writeBlob(count++, inputStream);
         }
     }
 }
