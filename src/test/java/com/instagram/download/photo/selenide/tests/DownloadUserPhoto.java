a0659f5cb34cf612cf314036f05ab9ac0a0d7933
Aleksey <loha.malyota@gmail.com>
2020-05-03 13:43:21 +0300

    use retry analyzer with TestNG.xml

--- src/test/java/com/instagram/download/photo/selenide/tests/DownloadUserPhoto.java
+++ src/test/java/com/instagram/download/photo/selenide/tests/DownloadUserPhoto.java
@@ -3,7 +3,6 @@ package com.instagram.download.photo.selenide.tests;
 import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import com.instagram.download.photo.databases.MariaDB;
 import com.instagram.download.photo.listeners.DatabaseListener;
-import com.instagram.download.photo.listeners.RetryListener;
 import com.instagram.download.photo.parameters.UserParameters;
 import com.instagram.download.photo.selenide.pages.LoginPage;
 import com.instagram.download.photo.selenide.pages.NewsPage;
@@ -22,7 +21,7 @@ public class DownloadUserPhoto {
         return new Object[][]{{new UserParameters("", "", "vkvisionary")}}; //yan_lapotkov
     }
 
-    @Test(dataProvider = "main", retryAnalyzer = RetryListener.class)
+    @Test(dataProvider = "main")
     public void testDownloadUserPhoto(UserParameters user) {
         LoginPage loginPage = new LoginPage();
         loginPage.tryToLogin(user.getUsername(), user.getPassword());
