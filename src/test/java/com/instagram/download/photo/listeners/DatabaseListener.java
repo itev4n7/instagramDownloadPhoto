57fb2d14b7e46f328cdae3b34edbc6a1e8d9f4b2
Aleksey <loha.malyota@gmail.com>
2020-05-13 17:09:08 +0300

    set binary chrome for jenkins

--- src/test/java/com/instagram/download/photo/listeners/DatabaseListener.java
+++ src/test/java/com/instagram/download/photo/listeners/DatabaseListener.java
@@ -2,6 +2,7 @@ package com.instagram.download.photo.listeners;
 
 import com.instagram.download.photo.connections.DatabaseConnection;
 import com.instagram.download.photo.databases.MariaDB;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.IExecutionListener;
 
 public class DatabaseListener implements IExecutionListener {
@@ -10,6 +11,8 @@ public class DatabaseListener implements IExecutionListener {
     public void onExecutionStart() {
         DatabaseConnection.openConnection();
         MariaDB.initTable();
+        ChromeOptions ch = new ChromeOptions();
+        ch.setBinary("/usr/bin/google-chrome");
     }
 
     @Override
