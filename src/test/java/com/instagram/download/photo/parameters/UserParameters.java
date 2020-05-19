da021b97db97e046762bcd89a77e03dc4d4b2381
Aleksey <loha.malyota@gmail.com>
2020-05-19 17:51:47 +0300

    refactor with builder pattern

--- src/test/java/com/instagram/download/photo/parameters/UserParameters.java
+++ src/test/java/com/instagram/download/photo/parameters/UserParameters.java
@@ -5,10 +5,35 @@ public class UserParameters {
     private final String password;
     private final String link;
 
-    public UserParameters(String username, String password, String link) {
-        this.username = username;
-        this.password = password;
-        this.link = link;
+    private UserParameters(Builder builder) {
+        this.username = builder.username;
+        this.password = builder.password;
+        this.link = builder.link;
+    }
+
+    public static class Builder {
+        private String username;
+        private String password;
+        private String link;
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder setLink(String link) {
+            this.link = link;
+            return this;
+        }
+
+        public UserParameters build() {
+            return new UserParameters(this);
+        }
     }
 
     public String getUsername() {
